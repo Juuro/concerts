6199539d2366b04c3547485b1af93b3cac3e0352
Sebastian Engel <sebastianengel@posteo.de>
2023-10-06 04:09:58 +0200

    fix: replace StaticQuery with useStaticQuery

--- src/components/image.js
+++ src/components/image.js
@@ -1,34 +1,20 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `StaticQuery`: https://gatsby.dev/staticquery
- */
-
-// TODO: Replace StaticQuery.
-const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
-    `}
-    render={(data) => (
-      <GatsbyImage image={data.placeholderImage.childImageSharp.fluid} />
-    )}
-  />
-)
+    }
+  `)
+
+  return <GatsbyImage image={data.placeholderImage.childImageSharp.fluid} />
+}
 export default Image
