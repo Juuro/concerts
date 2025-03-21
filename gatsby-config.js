b3c98ce4ffbc7f540ba25f5caea77f44396c7b2f
Sebastian Engel <sebastianengel@posteo.de>
2025-03-22 00:24:22 +0100

    chore: dependency updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -56,5 +54,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 }
